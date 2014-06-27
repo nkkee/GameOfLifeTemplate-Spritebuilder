c88a9f3383c84d31c189919d4cd878c4a9b1d3ad
Nicholas Kee <nicholaskee@Nicholass-Mac.local>
2014-06-27 10:15:22 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -81,7 +81,7 @@ static const int GRID_COLUMNS = 10;
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
     //get the row and column that was touched, return the Creature inside the corresponding cell
-    return _gridArray[2][3];
+    return _gridArray[][];
 }
 
 @end
