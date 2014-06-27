36718484b740cc1a7b6aba9929be284560f4d140
Nicholas Kee <nicholaskee@Nicholass-Mac.local>
2014-06-27 09:51:56 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -14,3 +14,21 @@
 @property (nonatomic, assign) int generation;
 
 @end
+
+@interface evelveStep: CCSprite
+
+//update each Creature's neighbor count
+[self countNeighbors];
+
+//update each Creature's state
+[self updateCreatures];
+
+//update the generation so the label's text will display the correct generation
+_generation++;
+
+@end
+
+@interface countNeighbors: CCSprite
+
+@end
+
